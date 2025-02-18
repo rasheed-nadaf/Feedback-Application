119468e178d2e126cca9c20e054cb0897c738bb6
Rasheed Nadaf <rasheednadaf4@gmail.com>
2025-02-19 00:31:54 +0530

    Updated API URL for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./index.css";
 
-const API_URL = "http://localhost:5000/feedback"; // Backend URL
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/feedback"; // Backend URL
 
 function FeedbackApp() {
     const [name, setName] = useState("");
